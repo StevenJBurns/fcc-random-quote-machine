71ce430a123c6af281e392f7e59189802edf232c
StevenJBurns <stevenjburns@msn.com>
2018-11-01 12:31:05 -0600

    Updates

---
+++ precache-manifest.f58d3fe9c6878f9163e3dc07f818e3f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b6fa26f7da9dfa9ce33",
+    "url": "/fcc-random-quote-machine/static/js/runtime~main.0b6fa26f.js"
+  },
+  {
+    "revision": "17dfb3e66974156045ac",
+    "url": "/fcc-random-quote-machine/static/js/main.17dfb3e6.chunk.js"
+  },
+  {
+    "revision": "4d34d0425c0e81d464a2",
+    "url": "/fcc-random-quote-machine/static/js/1.4d34d042.chunk.js"
+  },
+  {
+    "revision": "17dfb3e66974156045ac",
+    "url": "/fcc-random-quote-machine/static/css/main.4bba918e.chunk.css"
+  },
+  {
+    "revision": "79333e0ac3bf01893ab0380d8e965802",
+    "url": "/fcc-random-quote-machine/index.html"
+  }
+];
