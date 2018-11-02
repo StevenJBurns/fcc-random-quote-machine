cc5d98eef360b79d652b2e38de55d9c686181da4
StevenJBurns <stevenjburns@msn.com>
2018-11-01 20:21:20 -0600

    Added object to hold color schemes for randomization

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,16 @@ import './styles/App.css';
 
 const urlQuotes = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
+const colorSchemes = {
+  red : [],
+  blue: [],
+  purple: [],
+  orange: [],
+  green: [],
+  brown: [],
+  gold: [],
+  grey: []
+}
 
 class App extends React.Component {
   constructor(props) {
