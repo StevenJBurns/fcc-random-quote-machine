c9c6b0d3962fa2b41bf1c37e15b6300d5567fc1f
StevenJBurns <stevenjburns@msn.com>
2018-11-03 00:29:48 -0600

    Layout for buttons is fixed; added h2 and h4 to header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,7 +3,11 @@ import React from "react";
 
 const AppHeader = (props) => {
   return (
-    <header id="app-header" style={{ backgroundColor: props.scheme["dark"] }}>{props.children}</header>
+    <header id="app-header" style={{ backgroundColor: props.scheme["dark"] }}>
+      <h2>Random Quotes</h2>
+      <h4>built for Free Code Camp</h4>
+      {props.children}
+    </header>
   );
 };
 
