60ce8f6484d53cf1ba57b6946c96808848974daa
StevenJBurns <stevenjburns@msn.com>
2018-11-03 07:19:02 -0600

    Renamed AppHeader h2

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -8,7 +8,7 @@ const AppHeader = (props) => {
   return (
     <header id="app-header" style={{ backgroundColor: props.scheme["dark"] }}>
       <div id="div-headings-wrapper">
-        <h2>Random Quotes</h2>
+        <h2>Random Quote Machine</h2>
         <h4>built for Free Code Camp</h4>  
       </div>
       <FontAwesomeIcon icon={ faFreeCodeCamp } className="faIcon" color="#F0F0F0" size="4x" />
