53cc3dd7ad9f6f81f0f4100acb69560f07d78ef7
StevenJBurns <stevenjburns@msn.com>
2018-11-03 02:34:30 -0600

    Added Free Code Camp logo to ApHeader via Font Awesome

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFreeCodeCamp } from "@fortawesome/free-brands-svg-icons"
+
 
 const AppHeader = (props) => {
   return (
     <header id="app-header" style={{ backgroundColor: props.scheme["dark"] }}>
       <h2>Random Quotes</h2>
       <h4>built for Free Code Camp</h4>
-      {props.children}
+      <FontAwesomeIcon icon={ faFreeCodeCamp } color="#F0F0F0" size="4x" />
     </header>
   );
 };
